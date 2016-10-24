b36b82269e75c8e1e259518219da2818b8a47642
Nick Fohs <nick.fohs@gmail.com>
2016-10-24 23:54:59 +0000

    Done.

--- index.js
+++ index.js
@@ -16,5 +16,14 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  return document.querySelector('#grand-node div div div div')
+  //return document.querySelector('#grand-node div div div div')
+  let childArray = document.getElementById('grand-node')
+  let next = childArray.children[0]
+
+  while (next) {
+    childArray = next
+    next = childArray.children[0]
+  }
+
+  return childArray
 }
